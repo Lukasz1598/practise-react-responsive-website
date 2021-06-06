d56ed754104d5b1c27ee839f1767f5baec4107bf
LukaszKraken <Lukasz1598@gmail.com>
2021-06-06 19:21:49 +0200

    small images

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,20 +3,20 @@ import CardItem from './CardItem';
 import './Cards.css';
 
 const Cards = () => {
-    return ( 
+    return (
         <div className="cards">
             <h1>Check out this places</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className='cards__items'>
                         <CardItem
-                            src='images/img-9.jpg'
+                            src='images/img-9-s.jpg'
                             text='Explore the hidden waterfall deep inside the Amazon Jungle'
                             label='Adventure'
                             path='/services'
                         />
                         <CardItem
-                            src='images/img-2.jpg'
+                            src='images/img-2-s.jpg'
                             text='Travel through the Islands of Bali in a Private Cruise'
                             label='Luxury'
                             path='/services'
@@ -24,19 +24,19 @@ const Cards = () => {
                     </ul>
                     <ul className='cards__items'>
                         <CardItem
-                            src='images/img-3.jpg'
+                            src='images/img-3-s.jpg'
                             text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
                             label='Mystery'
                             path='/services'
                         />
                         <CardItem
-                            src='images/img-4.jpg'
+                            src='images/img-4-s.jpg'
                             text='Experience Football on Top of the Himilayan Mountains'
                             label='Adventure'
                             path='/products'
                         />
                         <CardItem
-                            src='images/img-8.jpg'
+                            src='images/img-8-s.jpg'
                             text='Ride through the Sahara Desert on a guided camel tour'
                             label='Adrenaline'
                             path='/sign-up'
@@ -45,7 +45,7 @@ const Cards = () => {
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Cards;
